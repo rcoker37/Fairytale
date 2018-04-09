d9249a6f64d584bd53cb7f5a0416df1db30a3b79
Ross Coker <rcoker37@gmail.com>
2018-04-09 12:20:28 -0400

    Added step sound

---
+++ Fairytale/Assets/Scripts/StepSoundController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepSoundController : MonoBehaviour {
+    public bool ShouldPlaySound;
+
+    private AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (ShouldPlaySound)
+        {
+            audio.Stop();
+            audio.Play();
+            ShouldPlaySound = false;
+        }	
+	}
+}
