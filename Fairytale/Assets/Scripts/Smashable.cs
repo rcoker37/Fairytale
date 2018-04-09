6a40a10a417b9528a28b7fded696f4fdce876a22
kaikue <kvk@pobox.com>
2018-04-09 12:02:33 -0400

    Add smashable objects

---
+++ Fairytale/Assets/Scripts/Smashable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smashable : MonoBehaviour {
+
+	public float SmashThreshold;
+
+	private Rigidbody2D rb;
+
+	private void Start()
+	{
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	private void OnCollisionEnter2D()
+	{
+		print(rb.velocity.y);
+		if (Mathf.Abs(rb.velocity.y) >= SmashThreshold)
+		{
+			Smash();
+		}
+	}
+
+	private void Smash()
+	{
+		print("SMASH");
+		//TODO: play sound
+		//TODO: leave broken bits
+		Destroy(gameObject);
+	}
+	
+}
