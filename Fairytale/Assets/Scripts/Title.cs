d246281580172b19ba0837989088c40a8288f8f0
Kai Kuehner <kvk@pobox.com>
2018-05-03 23:22:29 -0400

    story

---
+++ Fairytale/Assets/Scripts/Title.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour {
+
+	public void NextLevel()
+	{
+		int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(sceneIndex + 1);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
