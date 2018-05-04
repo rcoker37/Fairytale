df73c4d50ff9fc5cced76a9c1b4763be4e15d274
rcoker37 <rcoker37@gmail.com>
2018-05-03 20:27:58 -0400

    Added team logo

---
+++ Fairytale/Assets/Scripts/TitleController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour {
+
+	public GameObject loadingOverlay;
+
+	public string FirstLevelName = "Hub";
+
+	private void Start()
+	{
+	}
+
+	public void Continue()
+	{
+		loadingOverlay.SetActive(true);
+	}
+
+	public void NewGame()
+	{
+
+		loadingOverlay.SetActive(true);
+		//load intro
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
